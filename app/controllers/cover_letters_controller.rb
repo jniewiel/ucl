1aefdcbabb09930d43c54e09043ba7558c768c66
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-07-25 21:35:48 +0000

    implemented json to generate loading placeholder + clean up cover letter format

--- app/controllers/cover_letters_controller.rb
+++ app/controllers/cover_letters_controller.rb
@@ -1,7 +1,7 @@
 # app/controllers/cover_letters_controller.rb
 
 class CoverLettersController < ApplicationController
-require_relative '../services/openai_service'
+  require_relative '../services/openai_service'
 
   def generate
     resume = params[:resume]
@@ -9,7 +9,10 @@ require_relative '../services/openai_service'
 
     openai_service = OpenaiService.new
     @cover_letter = openai_service.generate_cover_letter(resume, job_description)
-    
-    render :new
+
+    respond_to do |format|
+      format.html { render :new }
+      format.json { render json: { cover_letter: @cover_letter.gsub("\n", "<br>") } }
+    end
   end
 end
