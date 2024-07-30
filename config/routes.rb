e20837388a7d80f4d591edc6dfb827c4df6a9da8
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-07-30 13:58:34 +0000

    edited routes for logged users and test trial

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root "cover_letters#new"
-  
+  root "home#index"
+
   resources :job_postings
   resources :cover_letters
   resources :resumes
 
+  get 'guest_login', to: 'home#guest_login'
+  get 'ucl_test', to: 'home#test'
   post '/generate_cover_letter', to: 'cover_letters#generate'
 end
