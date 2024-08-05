7a14d53fb599f1abf65aef776e824ccd5fdb672f
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-08-05 17:48:17 +0000

    corrected migrations

--- db/migrate/20240729190248_create_cover_letters.rb
+++ db/migrate/20240729190248_create_cover_letters.rb
@@ -1,12 +1,9 @@
 class CreateCoverLetters < ActiveRecord::Migration[7.1]
   def change
     create_table :cover_letters do |t|
-      # t.datetime :created_at
-      # t.datetime :updated_at
       t.references :user, null: false, foreign_key: true
       t.references :job_posting, null: false, foreign_key: true
       t.text :cover_letter_text
-
       t.timestamps
     end
   end
--- db/migrate/20240805174230_create_job_postings.rb
+++ db/migrate/20240805174230_create_job_postings.rb
@@ -1,11 +1,8 @@
 class CreateJobPostings < ActiveRecord::Migration[7.1]
   def change
     create_table :job_postings do |t|
-      # t.datetime :created_at
-      # t.datetime :updated_at
-      t.text :job_posting
+      t.string :job_posting
       t.text :job_description
-
       t.timestamps
     end
   end
