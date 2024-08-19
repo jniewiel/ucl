f3d078d412d0685c3d34290b18e62a1d53f608e8
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-08-19 17:24:53 +0000

    secured application by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
-# <!-- app/controllers/application_controller.rb -->
+# app/controllers/application_controller.rb
 
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
+  include Pundit::Authorization
+  
+  # Raises an exception if the CSRF token doesn't match.
+  protect_from_forgery with: :exception
+  
+  # Catches any authorization errors thrown by Pundit and handles them.
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  # Catches any "record not found" errors and handles them.
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def user_not_authorized(exception)
+    # Accepts an exception parameter, which allows us to get more information about the authorization failure.
+    policy_name = exception.policy.class.to_s.underscore
+    
+    # Translates the error message using the Pundit I18n (internationalization) translations.
+    flash[:alert] = t("#{policy_name}.#{exception.query}", scope: "pundit", default: "You are not authorized to perform this action.")
+    redirect_back(fallback_location: root_path)
+  end
+
+  # Handle cases where a requested record doesn't exist.
+  def record_not_found
+    flash[:alert] = "The requested resource could not be found."
+    redirect_to(request.referrer || root_path)
+  end
 end
